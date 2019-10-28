c8427bf1e51210b9684631173ffb4bc88b81b3bf
limq <limq@cgnfs.com.cn>
2019-10-28 17:38:17 +0800

    why need reflect 2

--- reflect/main.go
+++ reflect/main.go
@@ -2,33 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(Sprint("asdvsadfd"))
-}
 
-func Sprint(x interface{}) string {
-	//type stringer interface {
-	//	String() string
-	//}
+	//t := reflect.TypeOf(3)
+	//fmt.Println(t.String())
+	//fmt.Println(t)
+	//
+	//var w io.Writer = os.Stdout
+	//fmt.Println(reflect.TypeOf(w))
+	//fmt.Printf("%T\n",w)
+	//fmt.Printf("%T\n",3)
 
-	switch x := x.(type) {
-	//case stringer:
-	//	return  x.String()
-	case string:
-		return x
-	case int:
-		return strconv.Itoa(x)
-	case bool:
-		if x {
-			return "true"
-		} else {
-			return "false"
-		}
-	default:
-		return "???"
-	}
+	v := reflect.ValueOf(3)
+	fmt.Println(v)
+	fmt.Printf("%v\n", v)
+	fmt.Println(v.String())
+
+	vs := reflect.ValueOf("abc")
+	fmt.Println(vs)
+	fmt.Printf("%v\n", vs)
+	fmt.Println(vs.String())
+
+	fmt.Println("=======")
+	t := v.Type()
+	fmt.Println(t.String())
+	ts := vs.Type()
+	fmt.Println(ts.String())
 
 }
