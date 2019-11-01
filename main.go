da90125c14b9e6f1b1990e5ba02e3d3e712d9154
limq <limq@cgnfs.com.cn>
2019-11-01 15:31:38 +0800

    Routing override context: add the context's methods 回头再看

--- main.go
+++ main.go
@@ -1,91 +1,103 @@
 package main
 
-// In this package I'll show you how to override the existing Context's functions and methods.
-// You can easly navigate to the custom-context example to see how you can add new functions
-// to your own context (need a custom handler).
-//
-// This way is far easier to understand and it's faster when you want to override existing methods:
 import (
-	"reflect"
+	"sync"
 
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/context"
+	"github.com/kataras/iris/v12/sessions"
 )
 
-// Create your own custom Context, put any fields you wanna need.
-type MyContext struct {
-	// Optional Part 1: embed (optional but required if you don't want to override all context's methods)
+// Owner is our application structure, it contains the methods or fields we need,
+// think it as the owner of our *Context.
+type Owner struct {
+	// define here the fields that are global
+	// and shared to all clients.
+	sessionsManager *sessions.Sessions
+}
+
+// this package-level variable "application" will be used inside context to communicate with our global Application.
+var owner = &Owner{
+	sessionsManager: sessions.New(sessions.Config{Cookie: "mysessioncookie"}),
+}
+
+// Context is our custom context.
+// Let's implement a context which will give us access
+// to the client's Session with a trivial `ctx.Session()` call.
+type Context struct {
 	iris.Context
+	session *sessions.Session
 }
 
-var _ iris.Context = &MyContext{} // optionally: validate on compile-time if MyContext implements context.Context.
+// Session returns the current client's session.
+func (ctx *Context) Session() *sessions.Session {
+	// this help us if we call `Session()` multiple times in the same handler
+	if ctx.session == nil {
+		// start a new session if not created before.
+		ctx.session = owner.sessionsManager.Start(ctx.Context)
+	}
 
-// The only one important if you will override the Context
-// with an embedded context.Context inside it.
-// Required in order to run the handlers via this "*MyContext".
-func (ctx *MyContext) Do(handlers context.Handlers) {
-	context.Do(ctx, handlers)
+	return ctx.session
 }
 
-// The second one important if you will override the Context
-// with an embedded context.Context inside it.
-// Required in order to run the chain of handlers via this "*MyContext".
-func (ctx *MyContext) Next() {
-	context.Next(ctx)
+// Bold will send a bold text to the client.
+func (ctx *Context) Bold(text string) {
+	ctx.HTML("<b>" + text + "</b>")
 }
 
-// Override any context's method you want...
-// [...]
+var contextPool = sync.Pool{New: func() interface{} {
+	return &Context{}
+}}
 
-func (ctx *MyContext) HTML(format string, args ...interface{}) (int, error) {
-	ctx.Application().Logger().Infof("Executing .HTML function from MyContext")
+func acquire(original iris.Context) *Context {
+	ctx := contextPool.Get().(*Context)
+	ctx.Context = original // set the context to the original one in order to have access to iris's implementation.
+	ctx.session = nil      // reset the session
+	return ctx
+}
 
-	ctx.ContentType("text/html")
-	return ctx.Writef(format, args...)
+func release(ctx *Context) {
+	contextPool.Put(ctx)
+}
+
+// Handler will convert our handler of func(*Context) to an iris Handler,
+// in order to be compatible with the HTTP API.
+func Handler(h func(*Context)) iris.Handler {
+	return func(original iris.Context) {
+		ctx := acquire(original)
+		h(ctx)
+		release(ctx)
+	}
+}
+
+func newApp() *iris.Application {
+	app := iris.New()
+
+	// Work as you did before, the only difference
+	// is that the original context.Handler should be wrapped with our custom
+	// `Handler` function.
+	app.Get("/", Handler(func(ctx *Context) {
+		ctx.Bold("Hello from our *Context")
+	}))
+
+	app.Post("/set", Handler(func(ctx *Context) {
+		nameFieldValue := ctx.FormValue("name")
+		ctx.Session().Set("name", nameFieldValue)
+		ctx.Writef("set session = " + nameFieldValue)
+	}))
+
+	app.Get("/get", Handler(func(ctx *Context) {
+		name := ctx.Session().GetString("name")
+		ctx.Writef(name)
+	}))
+
+	return app
 }
 
 func main() {
-	app := iris.New()
-	// app.Logger().SetLevel("debug")
+	app := newApp()
 
-	// The only one Required:
-	// here is how you define how your own context will
-	// be created and acquired from the iris' generic context pool.
-	app.ContextPool.Attach(func() iris.Context {
-		return &MyContext{
-			// Optional Part 3:
-			Context: context.NewContext(app),
-		}
-	})
-
-	// Register a view engine on .html files inside the ./view/** directory.
-	app.RegisterView(iris.HTML("./static", ".html"))
-
-	// register your route, as you normally do
-	app.Handle("GET", "/", recordWhichContextJustForProofOfConcept, func(ctx iris.Context) {
-		// use the context's overridden HTML method.
-		ctx.HTML("<h1> Hello from my custom context's HTML! </h1>")
-	})
-
-	// this will be executed by the MyContext.Context
-	// if MyContext is not directly define the View function by itself.
-	app.Handle("GET", "/hi/{firstname:alphabetical}", recordWhichContextJustForProofOfConcept, func(ctx iris.Context) {
-		firstname := ctx.Values().GetString("firstname")
-
-		ctx.ViewData("firstname", firstname)
-		ctx.Gzip(true)
-
-		ctx.View("hi.html")
-	})
-
+	// GET: http://localhost:8080
+	// POST: http://localhost:8080/set
+	// GET: http://localhost:8080/get
 	app.Run(iris.Addr(":8080"))
 }
-
-// should always print "($PATH) Handler is executing from 'MyContext'"
-func recordWhichContextJustForProofOfConcept(ctx iris.Context) {
-	ctx.Values().Set("firstname", ctx.Params().Get("firstname")) //add this line
-	ctx.Application().Logger().Infof("(%s) Handler is executing from: '%s'", ctx.Path(), reflect.TypeOf(ctx).Elem().Name())
-	ctx.Next()
-}
-
-// Look "new-implementation" to see how you can create an entirely new Context with new functions.
