11f1ae3601ed85540f84171dffa6c8917fe643a0
limq <limq@cgnfs.com.cn>
2019-11-01 15:05:02 +0800

    Routing override context: override the context's methods

--- main.go
+++ main.go
@@ -1,69 +1,91 @@
 package main
 
+// In this package I'll show you how to override the existing Context's functions and methods.
+// You can easly navigate to the custom-context example to see how you can add new functions
+// to your own context (need a custom handler).
+//
+// This way is far easier to understand and it's faster when you want to override existing methods:
 import (
-	"net/http"
-	"strings"
+	"reflect"
 
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
 )
 
-// In this example you'll just see one use case of .WrapRouter.
-// You can use the .WrapRouter to add custom logic when or when not the router should
-// be executed in order to execute the registered routes' handlers.
-func newApp() *iris.Application {
-	app := iris.New()
+// Create your own custom Context, put any fields you wanna need.
+type MyContext struct {
+	// Optional Part 1: embed (optional but required if you don't want to override all context's methods)
+	iris.Context
+}
 
-	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
-		ctx.HTML("<b>Resource Not found</b>")
-	})
+var _ iris.Context = &MyContext{} // optionally: validate on compile-time if MyContext implements context.Context.
 
-	app.Get("/profile/{username}", func(ctx iris.Context) {
-		ctx.Writef("Hello %s", ctx.Params().Get("username"))
-	})
+// The only one important if you will override the Context
+// with an embedded context.Context inside it.
+// Required in order to run the handlers via this "*MyContext".
+func (ctx *MyContext) Do(handlers context.Handlers) {
+	context.Do(ctx, handlers)
+}
 
-	app.HandleDir("/", "./public")
+// The second one important if you will override the Context
+// with an embedded context.Context inside it.
+// Required in order to run the chain of handlers via this "*MyContext".
+func (ctx *MyContext) Next() {
+	context.Next(ctx)
+}
 
-	myOtherHandler := func(ctx iris.Context) {
-		ctx.Writef("inside a handler which is fired manually by our custom router wrapper")
-	}
+// Override any context's method you want...
+// [...]
 
-	// wrap the router with a native net/http handler.
-	// if url does not contain any "." (i.e: .css, .js...)
-	// (depends on the app , you may need to add more file-server exceptions),
-	// then the handler will execute the router that is responsible for the
-	// registered routes (look "/" and "/profile/{username}")
-	// if not then it will serve the files based on the root "/" path.
-	app.WrapRouter(func(w http.ResponseWriter, r *http.Request, router http.HandlerFunc) {
-		path := r.URL.Path
+func (ctx *MyContext) HTML(format string, args ...interface{}) (int, error) {
+	ctx.Application().Logger().Infof("Executing .HTML function from MyContext")
 
-		if strings.HasPrefix(path, "/other") {
-			// acquire and release a context in order to use it to execute
-			// our custom handler
-			// remember: we use net/http.Handler because here we are in the "low-level", before the router itself.
-			ctx := app.ContextPool.Acquire(w, r)
-			myOtherHandler(ctx)
-			app.ContextPool.Release(ctx)
-			return
-		}
-
-		router.ServeHTTP(w, r) // else continue serving routes as usual.
-	})
-
-	return app
+	ctx.ContentType("text/html")
+	return ctx.Writef(format, args...)
 }
 
 func main() {
-	app := newApp()
+	app := iris.New()
+	// app.Logger().SetLevel("debug")
 
-	// http://localhost:8080
-	// http://localhost:8080/index.html
-	// http://localhost:8080/app.js
-	// http://localhost:8080/css/main.css
-	// http://localhost:8080/profile/anyusername
-	// http://localhost:8080/other/random
+	// The only one Required:
+	// here is how you define how your own context will
+	// be created and acquired from the iris' generic context pool.
+	app.ContextPool.Attach(func() iris.Context {
+		return &MyContext{
+			// Optional Part 3:
+			Context: context.NewContext(app),
+		}
+	})
+
+	// Register a view engine on .html files inside the ./view/** directory.
+	app.RegisterView(iris.HTML("./static", ".html"))
+
+	// register your route, as you normally do
+	app.Handle("GET", "/", recordWhichContextJustForProofOfConcept, func(ctx iris.Context) {
+		// use the context's overridden HTML method.
+		ctx.HTML("<h1> Hello from my custom context's HTML! </h1>")
+	})
+
+	// this will be executed by the MyContext.Context
+	// if MyContext is not directly define the View function by itself.
+	app.Handle("GET", "/hi/{firstname:alphabetical}", recordWhichContextJustForProofOfConcept, func(ctx iris.Context) {
+		firstname := ctx.Values().GetString("firstname")
+
+		ctx.ViewData("firstname", firstname)
+		ctx.Gzip(true)
+
+		ctx.View("hi.html")
+	})
+
 	app.Run(iris.Addr(":8080"))
-
-	// Note: In this example we just saw one use case,
-	// you may want to .WrapRouter or .Downgrade in order to bypass the iris' default router, i.e:
-	// you can use that method to setup custom proxies too.
 }
+
+// should always print "($PATH) Handler is executing from 'MyContext'"
+func recordWhichContextJustForProofOfConcept(ctx iris.Context) {
+	ctx.Values().Set("firstname", ctx.Params().Get("firstname")) //add this line
+	ctx.Application().Logger().Infof("(%s) Handler is executing from: '%s'", ctx.Path(), reflect.TypeOf(ctx).Elem().Name())
+	ctx.Next()
+}
+
+// Look "new-implementation" to see how you can create an entirely new Context with new functions.
