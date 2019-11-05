d0ecf8e77fb348e1d8429c2aa360d3dbe274be57
limq <limq@cgnfs.com.cn>
2019-11-05 17:31:12 +0800

    mvc hello world

--- main.go
+++ main.go
@@ -2,59 +2,34 @@ package main
 
 import (
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/logger"
 	"github.com/kataras/iris/v12/middleware/recover"
 	"github.com/kataras/iris/v12/mvc"
+	"github.com/kataras/iris/v12/sessions"
 )
 
 func main() {
-	app := newApp()
+	app := iris.New()
+	app.Use(recover.New())
+	app.Logger().SetLevel("debug")
+
+	mvc.Configure(app.Party("/basic"), basicMvc)
+
 	app.Run(iris.Addr(":8080"))
 }
 
-func newApp() *iris.Application {
-	app := iris.New()
-	app.Use(recover.New())
-	app.Use(logger.New())
+func basicMvc(app *mvc.Application) {
+	//使用中间件
+	app.Router.Use(func(ctx iris.Context) {
+		ctx.Application().Logger().Infof("Path:%s", ctx.Path())
+		ctx.Next()
+	})
+	//
+	app.Register(
+		sessions.New(sessions.Config{}).Start,
+	)
 
-	mvc.New(app).Handle(new(ExampleController))
-	return app
-}
+	//Handle控制器
+	app.Handle(new(basicController))
+	app.Party("/sub").Handle(new(basicSubController))
 
-type ExampleController struct{}
-
-func (c *ExampleController) Get() mvc.Result {
-	return mvc.Response{
-		ContentType: "text/html",
-		Text:        "<h1>welcome</h1>",
-	}
-}
-
-func (c *ExampleController) GetPing() string {
-	return "pong"
-}
-
-func (c *ExampleController) GetHello() interface{} {
-	return map[string]string{"message": "hello iris!"}
-}
-
-func (c *ExampleController) GetUserName() interface{} {
-	return map[string]string{"username": "limq!"}
-}
-
-func (c *ExampleController) BeforeActivation(b mvc.BeforeActivation) {
-	//anyMiddlewareHere := func(ctx iris.Context){
-	//	ctx.Application().Logger().Warnf("Inside / custom_path")
-	//	ctx.Next()
-	//}
-	b.Handle("GET", "/custom_path", "CustomHandlerWithoutFollowingTheNamingGuide", anyMiddlewareHere)
-}
-
-func (c ExampleController) CustomHandlerWithoutFollowingTheNamingGuide() string {
-	return "hello from custom handler without following the naming guide"
-}
-
-func anyMiddlewareHere(ctx iris.Context) {
-	ctx.Application().Logger().Warnf("Inside / custom_path")
-	ctx.Next()
 }
